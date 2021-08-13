8d709259f388beee73e8ce3c3bd0a78c4f51b6ef
LAPTOP-0M00PN3C\Ali Iktider Sayam <aisayam.sayam@gmail.com>
2021-08-14 00:09:41 +0600

    app icon

--- client/src/components/Posts/posts.js
+++ client/src/components/Posts/posts.js
@@ -13,8 +13,12 @@ const Posts = () => {
 
      return (
           !posts.length ? <CircularProgress /> : (
-               <Grid className={classes.container}>
-                    
+               <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                    {posts.map((post) => (
+                         <Grid key={post._id} item xs={12} sm={6}>
+                              <Post post={post} />
+                         </Grid>
+                    ))}
                </Grid>
           )
      );
